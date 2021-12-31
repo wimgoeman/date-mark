a43f137f07b17333d04e3531f5dfa60cb41b4b93
Wim Goeman <wimgoeman@gmail.com>
2021-12-31 14:10:20 +0100

    feat: transaction log

--- main.go
+++ main.go
@@ -6,32 +6,28 @@ import (
 	"log"
 	"path/filepath"
 	"strings"
-	"sync"
 )
+
+var transactionLog *TransactionLog
 
 func main() {
 	log.Println("Starting")
 
 	pathFlag := flag.String("path", ".", "path to scan for pictures")
-	threadsFlag := flag.Int("threads", 4, "parallel threads to use")
+	trxFlag := flag.String("transactions", "", "path to transaction log")
+
 	flag.Parse()
 
-	start(*pathFlag, *threadsFlag)
+	start(*pathFlag, *trxFlag)
 
 	log.Println("Done")
 }
 
-func start(path string, threads int) {
+func start(path string, trxPath string) {
+	transactionLog = openTransactionLog(trxPath)
+	defer transactionLog.close()
 
-	waitGroup := sync.WaitGroup{}
-	pathChan := make(chan string)
-
-	for i := 0; i < threads; i++ {
-		waitGroup.Add(1)
-		go worker(&waitGroup, i, pathChan)
-	}
-
-	filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+	filepath.WalkDir(path, func(path string, d fs.DirEntry, _ error) error {
 		if d.IsDir() {
 			return nil
 		}
@@ -41,29 +37,28 @@ func start(path string, threads int) {
 			return nil
 		}
 
-		pathChan <- path
+		if transactionLog.find(path) != nil {
+			return nil
+		}
+
+		text, err := processFile(path)
+		var errText string
+		if err == nil {
+			errText = ""
+		} else {
+			errText = err.Error()
+		}
+		transactionLog.addTransaction(path, err == nil, text, errText)
 		return nil
 	})
-
-	close(pathChan)
-	waitGroup.Wait()
 }
 
-func worker(waitGroup *sync.WaitGroup, id int, pathChan chan string) {
-	defer waitGroup.Done()
-
-	for path := range pathChan {
-		processFile(path)
-	}
-}
-
-func processFile(path string) {
+func processFile(path string) (string, error) {
 	t, h, err := readInfoFromImage(path)
 	if err != nil {
-		log.Println(path, err)
-		return
+		return "", err
 	}
 	text := t.Format("02 Jan 06 15:04:05")
-	log.Println(path, text)
-	addTextToImage(path, path+".2.jpg", text, h/20)
+	err = addTextToImage(path, path, text, h/20)
+	return text, err
 }
---
+++ transactionlog.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Transaction struct {
+	path         string
+	date         string
+	success      bool
+	errorMessage string
+}
+
+type TransactionLog struct {
+	transactions []Transaction
+	file         *os.File
+	writer       *csv.Writer
+}
+
+func openTransactionLog(path string) *TransactionLog {
+	transactionLog := TransactionLog{
+		transactions: make([]Transaction, 0),
+	}
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Panic("Failed to open transaction log,", err)
+	}
+	transactionLog.file = f
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		log.Panic("Failed to seek to start of transaction log,", err)
+	}
+
+	r := csv.NewReader(f)
+
+	recs, err := r.ReadAll()
+	if err != nil {
+		log.Panic("Failed to parse transaction log,", err)
+	}
+
+	for i, rec := range recs {
+		if len(rec) < 4 {
+			log.Panic("Transaction log record", i, "too short")
+		}
+		path := rec[0]
+		success, err := strconv.ParseBool(rec[1])
+		if err != nil {
+			log.Panic("Failed to parse transaction log record", i, err)
+		}
+		date := rec[2]
+		errorMessage := rec[3]
+
+		trx := Transaction{
+			path:         path,
+			success:      success,
+			date:         date,
+			errorMessage: errorMessage,
+		}
+
+		transactionLog.transactions = append(transactionLog.transactions, trx)
+	}
+
+	transactionLog.file = f
+	transactionLog.writer = csv.NewWriter(f)
+	return &transactionLog
+}
+
+func (t *TransactionLog) addTransaction(path string, success bool, date string, errorMessage string) {
+
+	trx := Transaction{
+		path:         path,
+		success:      success,
+		date:         date,
+		errorMessage: errorMessage,
+	}
+	t.transactions = append(t.transactions, trx)
+	t.writer.Write([]string{
+		path, strconv.FormatBool(success), date, errorMessage,
+	})
+	t.writer.Flush()
+}
+
+func (t *TransactionLog) find(path string) *Transaction {
+	for _, trx := range t.transactions {
+		if trx.path == path {
+			return &trx
+		}
+	}
+	return nil
+}
+
+func (t *TransactionLog) close() {
+	t.file.Close()
+}
